cf6465671cbf5b789c2ce3bdf2e4f29d4da590b6
LunaB28 <lunab2815@gmail.com>
2025-05-04 01:54:41 +0000

    final commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,45 @@
 import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [toDo, setToDo] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+	const Delete = (indexToDelete) => {
+		const updatedToDo = [...toDo];
+		updatedToDo.splice(indexToDelete, 1);
+		setToDo(updatedToDo);
+	};
 	return (
-		<div className="text-center">
-            
+		<div className="text-center container mt-5">
+			<h1 className="p-4 mb-4 d-inline-flex rounded-4">To Do list</h1>
+			<input
+				className="form-control"
+				placeholder="añadir nuevo ToDo"
+				onChange={(event) => setInputValue(event.target.value)}
+				onKeyDown={(event) => {
+					if (event.key === "Enter" && inputValue.trim() !== "") {
+						setToDo([...toDo, inputValue]);
+						setInputValue("");
+					}
+				}}
+				value={inputValue}
+			/>
+			<ul className="list-group mx-5 px-5 my-3">
+				{toDo.map((item, index) => (
+					<li key={index} className="list-group-item d-flex justify-content-center">
+						<span className="flex-grow-1">{item}</span>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+						<button
+							className="btn btn-danger btn-sm"
+							onClick={() => Delete(index)} // Llama a la función para eliminar
+						>
+							X
+						</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
